1426142b00a37b46117cd1ff88d3a432fed56fad
VuDej <93735137+VuDej@users.noreply.github.com>
2022-01-15 21:48:47 +0100

    errors

--- src/index.js
+++ src/index.js
@@ -1,39 +1,38 @@
- import './style.css';
- import '@fortawesome/fontawesome-free/js/fontawesome.js';
- import '@fortawesome/fontawesome-free/js/solid.js';
- import '@fortawesome/fontawesome-free/js/regular.js';
- import '@fortawesome/fontawesome-free/js/brands.js';
+import './style.css';
+import '@fortawesome/fontawesome-free/js/fontawesome.js';
+import '@fortawesome/fontawesome-free/js/solid.js';
+import '@fortawesome/fontawesome-free/js/regular.js';
+import '@fortawesome/fontawesome-free/js/brands.js';
 
- import ToDo from './constructor.js';
- import renderList from './render.js';
+import ToDo from './constructor.js';
+import renderList from './render.js';
 
- let listToDo = new ToDo();
- renderList(listToDo);
+let listToDo = new ToDo();
+renderList(listToDo);
 
- const addBtn = document.querySelector('.add');
- const refresh = document.querySelector('.refresh');
+const addBtn = document.querySelector('.add');
+const refresh = document.querySelector('.refresh');
 
- addBtn.onclick = () => {
-   const description = document.querySelector('#input').value;
-   const completed = false;
-   const index = listToDo.list.length + 1;
-   const newTask = { description, completed, index };
+addBtn.onclick = () => {
+  const description = document.querySelector('#input').value;
+  const completed = false;
+  const index = listToDo.list.length + 1;
+  const newTask = { description, completed, index };
 
-   if (description) {
-     document.querySelector('#input').value = '';
-     listToDo.addToDo(newTask);
-     renderList(listToDo);
-   }
- };
+if (description) {
+  document.querySelector('#input').value = '';
+  listToDo.addToDo(newTask);
+  renderList(listToDo);
+  }
+};
 
- const clearBtn = document.querySelector('.clear-all');
-  clearBtn.addEventListener('click', () => {
+const clearBtn = document.querySelector('.clear-all');
+clearBtn.addEventListener('click', () => {
   listToDo.clearCompleted();
-  render(listToDo);
+  renderList(listToDo);
 });
 
- refresh.onclick = () => {
+refresh.onclick = () => {
   listToDo = new ToDo();
-   renderList(listToDo);
-  };
-
+  renderList(listToDo);
+};
