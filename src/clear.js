9a721d361cb706f6f5756b4882c4b33eb96e86df
VuDej <93735137+VuDej@users.noreply.github.com>
2022-01-15 22:18:44 +0100

    linters

--- src/clear.js
+++ src/clear.js
@@ -1,7 +1,7 @@
 const completedTasks = (list, todoId, status) => {
   const Update = list;
   const selected = list.findIndex((element) => element.id === todoId);
- Update[selected].completed = status;
+  Update[selected].completed = status;
   localStorage.setItem('toDoTask', JSON.stringify(Update));
 };
 export default completedTasks;
