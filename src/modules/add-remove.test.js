63d9588c5376ad3544ecd948556c64d43384a449
VuDej <93735137+VuDej@users.noreply.github.com>
2022-01-20 18:25:33 +0100

    test, add, remove, dom, passed

--- src/modules/add-remove.test.js
+++ src/modules/add-remove.test.js
@@ -1,26 +1,11 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import Dom from './mock-html.js';
 import ToDo from './constructor.js';
 import LocalStorage from './mock-storage.js';
-// document.body.innerHTML = `
-
-// <section class="main container">
-// <ul class="first">
-//   <li>
-//     <span class="left">Today's To Do</span>
-//     <button class="right refresh" ><i class="white fas fa-sync-alt"></i></button>
-//   </li>
-// </ul>
-// <form onSubmit="return false">
-//   <input id="input" type="text" placeholder="Add to your list...">
-//   <button class="add" type="submit">
-//         <i class="far fa-share-square fa-rotate-180"></i>
-//       </button>
-// </form>
-// <div id='drag' class="tasks"></div>
-// <button class='grey clear-all'>Clear all Completed</button>
-// </section>
-  
-// `;
-
+import renderList from './render'
 global.localStorage = new LocalStorage();
 
 const toDo = new ToDo();
@@ -34,12 +19,12 @@ describe('testing Add function', () => {
     test('testing description', () => {
       toDo.addToDo({ description: 'do something' });
       expect(toDo.list[1].description).toBe('do something');
-    });
+   });
   });
 
-//    describe('testing remove function', () => {
-//      it('testing if one toDo is removed', () => {
-//        toDo.removeToDo(1);
-//        expect(toDo.list.length).toBe(1);
-//      });
-//    });
+describe('testing remove function', () => {
+  test('testing if one toDo is removed', () => {
+    toDo.removeToDo();
+    expect(toDo.list.length).toBe(0);
+    });
+ });
