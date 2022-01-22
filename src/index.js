547382fae87f8e02c825ec24ceb671d47439f824
VuDej <93735137+VuDej@users.noreply.github.com>
2022-01-22 13:38:11 +0100

    error

--- src/index.js
+++ src/index.js
@@ -6,15 +6,13 @@ import '@fortawesome/fontawesome-free/js/brands.js';
 
 import renderList from './modules/render.js';
 import ToDo from './modules/constructor.js';
-import Sortable from 'sortablejs';
-import dragArea from './modules/dragging.js';
+import Sortable from './modules/dragging.js';
 
 let listToDo = new ToDo();
 renderList(listToDo);
 
 const addBtn = document.querySelector('.add');
 const refresh = document.querySelector('.refresh');
-
 
 addBtn.onclick = () => {
   const description = document.querySelector('#input').value;
@@ -29,7 +27,7 @@ clearBtn.onclick = () => {
   renderList(listToDo);
 };
 
-dragArea = new Sortable(dragArea, {
+ Sortable(dragArea, {
   handle: '.dragging', animation: 150,
 });
 
--- src/modules/dragging.js
+++ src/modules/dragging.js
@@ -1,4 +1,7 @@
+import Sortable from "sortablejs";
+
 const tasksToDo = document.querySelector('#drag');
 const dragArea = tasksToDo;
+const Sortable = new Sortable();
 
 export default dragArea;
